16db2f7fdba118c5db1e5184d9c907742972d16f
leonardoigor <igor_mendonca@outlook.com.br>
2021-10-03 15:54:11 -0300

    filme class

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,22 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController:ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionarFilme([FromBody] Filme filme) => filmes.Add(filme);
+
+        [HttpGet]
+        public IEnumerable<Filme> PegarFilmes() => filmes;
+    }
+}
---
+++ FilmesApi/Models/Filme.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesApi.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="required")]
+        public string Titulo { get; set;}
+        [Required(ErrorMessage = "required")]
+        public string Diretor { get; set;}
+        public string Genero { get; set; }
+        [Range(1,600,ErrorMessage ="Range")]
+        public int Duracao { get; set; }
+    }
+}
